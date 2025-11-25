79c19b6db195b9563227bbf4a7e6dfd09b8c67a9
adarshkaun <adarshkaun@gmail.com>
2025-11-25 22:33:01 +0530

    Q89

---
+++ Q89.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char str[200], ch;
+    int i = 0, count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Enter a character to find frequency: ");
+    scanf("%c", &ch);
+
+    while (str[i] != '\0') {
+        if (str[i] == ch)
+            count++;
+        i++;
+    }
+
+    printf("Frequency = %d\n", count);
+
+    return 0;
+}
