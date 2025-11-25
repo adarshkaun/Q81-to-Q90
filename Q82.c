b488fdf3343983fc4483b5801de88c828d38d71f
adarshkaun <adarshkaun@gmail.com>
2025-11-25 22:12:43 +0530

    Q82

---
+++ Q82.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0') {
+        if (str[i] != '\n')       // ignore newline stored by fgets
+            printf("%c\n", str[i]);
+        i++;
+    }
+
+    return 0;
+}
